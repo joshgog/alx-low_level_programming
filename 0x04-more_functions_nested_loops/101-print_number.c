03212679d17d6f6d2e50cc3c830e9e70732cc521
Joshua Ouma <joshuaoumag@gmail.com>
2022-12-17 05:54:53 +0300

    update code

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -9,18 +9,27 @@
 * Return: void
 */
 
+
 void print_number(int n)
 {
-	if (n < 0)
+
+	unsigned int n1 = 0;
+
+	if  (n < 0)
 	{
+		n1 = -n;
 		_putchar('-');
-		n = -n;
 	}
 
-	if (n / 10 > 0)
+	else
 	{
-	print_number(n / 10);
+		n1 = n;
 	}
 
-	_putchar(n % 10 + '0');
+	if (n1 / 10)
+	{
+		print_number(n1 / 10);
+	}
+
+	_putchar((n1 % 10) + '0');
 }
